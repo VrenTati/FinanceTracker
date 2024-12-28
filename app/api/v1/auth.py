b9b4685abf6c9cf417db1c89023bc0923851b5a4
VrenTati <kateryna.vynokurova.pz.2022@lpnu.ua>
2024-12-28 23:03:37 +0200

    Add current_user dependencies

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.dependecies.auth.backend import authentication_backend
+from api.dependecies.auth import authentication_backend
 from api.v1.fastapi_users import fastapi_users
 from core.config import settings
 from core.schemas.user import UserRead, UserCreate
@@ -16,4 +16,8 @@ router.include_router(
 
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
+)
+
+router.include_router(
+    fastapi_users.get_reset_password_router(),
 )
--- app/api/v1/fastapi_users.py
+++ app/api/v1/fastapi_users.py
@@ -1,7 +1,7 @@
 from fastapi_users import FastAPIUsers
 
-from api.dependecies.auth.backend import authentication_backend
-from api.dependecies.auth.user_manager import get_user_manager
+from api.dependecies.auth import authentication_backend
+from api.dependecies.auth import get_user_manager
 from core.models import User
 from core.types.user_id import UserIdType
 
@@ -9,3 +9,6 @@ fastapi_users = FastAPIUsers[User, UserIdType](
     get_user_manager,
     [authentication_backend],
 )
+
+current_user = fastapi_users.current_user(active=True)
+current_super_user = fastapi_users.current_user(active=True, superuser=True)
