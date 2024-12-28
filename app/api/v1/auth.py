bb118bd2aa6eb6481573c89d7197cb83cfdb1792
VrenTati <kateryna.vynokurova.pz.2022@lpnu.ua>
2024-12-28 18:43:16 +0200

    Add users router

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from api.dependecies.auth.backend import authentication_backend
 from api.v1.fastapi_users import fastapi_users
 from core.config import settings
+from core.schemas.user import UserRead, UserCreate
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
@@ -11,4 +12,8 @@ router = APIRouter(
 
 router.include_router(
     fastapi_users.get_auth_router(authentication_backend),
+)
+
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
 )
---
+++ app/api/v1/users.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from api.v1.fastapi_users import fastapi_users
+from core.config import settings
+from core.schemas.user import UserRead, UserUpdate
+
+router = APIRouter(
+    prefix=settings.api.v1.user,
+    tags=["users"],
+)
+
+router.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+)
