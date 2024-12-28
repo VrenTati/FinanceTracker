042ca6dee0b6e40121b490488b9315659e17bec7
VrenTati <kateryna.vynokurova.pz.2022@lpnu.ua>
2024-12-28 12:51:07 +0200

    create user id type

---
+++ app/core/models/user.py
@@ -0,0 +1,13 @@
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import Base
+from core.types.user_id import UserIdType
+from .mixins.id_int_pk_mixin import IdIntPKMixin
+
+class User(Base, IdIntPKMixin, SQLAlchemyBaseUserTable[UserIdType]):
+    pass
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, User)
