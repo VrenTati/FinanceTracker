9f3c42f01a367cc50ceddb0766f7bd9436de5508
VrenTati <kateryna.vynokurova.pz.2022@lpnu.ua>
2024-12-29 00:51:09 +0200

    Add models category and transaction

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -2,10 +2,14 @@ __all__ = (
     "db_helper",
     "Base",
     "User",
-    "AccessToken"
+    "AccessToken",
+    "Category",
+    "Transaction",
 )
 
 from .access_token import AccessToken
 from .base import Base
+from .category import Category
 from .db_helper import db_helper
+from .transaction import Transaction
 from .user import User
---
+++ app/core/models/category.py
@@ -0,0 +1,19 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core.models.base import Base
+
+if TYPE_CHECKING:
+    from core.models.transaction import Transaction
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+
+    transactions: Mapped[list["Transaction"]] = relationship(
+        "Transaction", back_populates="category", lazy="selectin"
+    )
