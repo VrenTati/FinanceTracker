d539516c7f90b7770e198cc3476bfb08b87482fe
VrenTati <kateryna.vynokurova.pz.2022@lpnu.ua>
2024-12-29 13:14:44 +0200

    Add schemas for transaction and category models

---
+++ app/core/schemas/category.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict
+
+class BaseCategory(BaseModel):
+    id: int
+    name: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+class BaseCategoryCreate(BaseModel):
+    name: str
+
+class BaseCategoryUpdate(BaseModel):
+    name: Optional[str] = None
