908e13284d53f3f582039daf37f5c6a1dd4777c9
VrenTati <kateryna.vynokurova.pz.2022@lpnu.ua>
2024-12-30 19:22:13 +0200

    Use black formatter

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import declared_attr
 
 from utils import camel_case_to_snake_case
 
+
 class Base(DeclarativeBase):
     __abstract__ = True
 
--- app/core/schemas/report.py
+++ app/core/schemas/report.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+
 class ReportFilter(BaseModel):
     start_date: Optional[date]
     end_date: Optional[date]
